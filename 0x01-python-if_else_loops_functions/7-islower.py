21d0ae2e2a1be966e89ac476d02a868759437cfe
Ire-oluwa <orodiji.ireoluwa@gmail.com>
2022-11-29 11:13:19 -0800

    checks if a character is lowercase

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,4 +3,4 @@ def islower(c):
     if ord(c) >= 97 and ord(c) <= 122:
         print("{:c} is lower".format(c), end="")
     else:
-        print("{:c} i upper".format(c), end="")
+        print("{:c} is upper".format(c), end="")
